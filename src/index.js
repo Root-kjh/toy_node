25eb694d3796582b289a3455c719b0f808b7b785
root-kjh <wldnro3141@gmail.com>
2022-12-08 06:40:57 +0900

    add nodemon

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ const Koa = require('koa');
 const app = new Koa();
 
 // response
-app.use(ctx => {
-    ctx.body = 'Hello Koa';
+app.use(async (ctx, next) => {
+    console.log(1);
+    const started = new Date();
+    await next();
+    console.log(new Date() - started + 'ms');
+    ctx.body = "test"
 });
 
 app.listen(3000);
